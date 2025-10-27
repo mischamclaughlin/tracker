e6c84e1dfb06c3fee1d76c58b6606894a2f15b71
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-10-27 12:14:20 +0000

    Split asset and transaction to allow clean update of balance

---
+++ app/models/asset.rb
@@ -0,0 +1,17 @@
+class Asset < ApplicationRecord
+  has_many :transactions, foreign_key: :asset, primary_key: :name
+
+  validates :name, presence: true, uniqueness: true
+  validates :balance, presence: true, numericality: true
+
+  def self.find_or_create_for(asset_name)
+    find_or_create_by(name: asset_name) do |asset|
+      asset.balance = 0
+    end
+  end
+
+  def recalculate_balance!
+    calculated_balance = transactions.sum("CASE WHEN action = 'buy' THEN amount WHEN action = 'sell' THEN -amount ELSE 0 END")
+    update_column(:balance, calculated_balance)
+  end
+end
