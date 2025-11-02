16a17d9061db90e018b64b12baf533bb1b2bf962
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-02 11:37:27 +0000

    Logigin module

--- lib/logging_module.rb
+++ lib/logging_module.rb
@@ -1,13 +1,16 @@
 module LoggingModule
   def log_error(message)
-    Rails.logger.error(message)
+    Rails.logger.error("ERROR: #{message}")
+    puts "ERROR: #{message}"
   end
 
   def log_info(message)
-    Rails.logger.info(message)
+    Rails.logger.info("INFO: #{message}")
+    puts "INFO: #{message}"
   end
 
   def log_debug(message)
-    Rails.logger.debug(message)
+    Rails.logger.debug("DEBUG: #{message}")
+    puts "DEBUG: #{message}"
   end
 end
