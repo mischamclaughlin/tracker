bf43c8a20c6618212801d04c4ce0238d1379280f
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-02 11:31:16 +0000

    Create spefic styling per column give classes for styling

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -3,17 +3,42 @@ module AssetsHelper
     content_tag(:table, class: 'assets-table') do
       concat(content_tag(:tr) do
         concat(content_tag(:th, 'Name'))
-        concat(content_tag(:th, 'Balance'))
+        concat(content_tag(:th, 'Asset Total'))
+        concat(content_tag(:th, 'Fiat Balance'))
+        concat(content_tag(:th, 'Current Price'))
+        concat(content_tag(:th, 'Total P&L'))
         concat(content_tag(:th, 'View'))
       end)
 
       assets.each do |asset|
         concat(content_tag(:tr) do
           concat(content_tag(:td, asset.name))
-          concat(content_tag(:td, number_to_currency(asset.balance)))
+          concat(content_tag(:td, format_crypto_amount(asset.balance), class: "numeric"))
+          concat(content_tag(:td, number_to_currency(asset.balance_in_fiat), class: "numeric"))
+          concat(content_tag(:td, number_to_currency(asset.current_price&.price || 0), class: "numeric"))
+          concat(content_tag(:td, content_tag(:div, display_pnl(asset.total_pnl), class: "#{asset.total_pnl.positive? ? 'positive' : 'negative'}")))
           concat(content_tag(:td, link_to('View', asset_path(asset))))
         end)
       end
     end
+  end
+
+  def format_crypto_amount(amount, precision: 8)
+    formatted = number_with_precision(amount, precision: precision, strip_insignificant_zeros: true)
+    formatted
+  end
+
+  def display_pnl(amount)
+    return content_tag(:p, "$0.00", class: 'pnl-neutral') if amount.zero?
+
+    css_class = amount.positive? ? 'pnl-positive' : 'pnl-negative'
+    content_tag(:p, number_to_currency(amount), class: css_class)
+  end
+
+  def display_pnl_percentage(percentage)
+    return content_tag(:p, "0.00%", class: 'pnl-neutral') if percentage.zero?
+
+    css_class = percentage.positive? ? 'pnl-positive' : 'pnl-negative'
+    content_tag(:p, "#{percentage.round(2)}%", class: css_class)
   end
 end
--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -7,18 +7,22 @@ module TransactionsHelper
         concat(content_tag(:th, 'Date'))
         concat(content_tag(:th, 'Time'))
         concat(content_tag(:th, 'Memo'))
-        concat(content_tag(:th, 'Amount'))
+        concat(content_tag(:th, 'Asset Amount'))
+        concat(content_tag(:th, 'Fiat Amount'))
+        concat(content_tag(:th, 'Price'))
         concat(content_tag(:th, 'View'))
       end)
 
       transactions.each do |transaction|
         concat(content_tag(:tr) do
           concat(content_tag(:td, transaction.asset))
-          concat(content_tag(:td, transaction.action, style: "background: #{transaction.action == 'buy' ? 'green' : 'red'}"))
-          concat(content_tag(:td, transaction.time.strftime('%Y-%m-%d')))
+          concat(content_tag(:td, content_tag(:div, transaction.action, class: "#{transaction.action == 'buy' ? 'positive' : 'negative'}")))
+          concat(content_tag(:td, transaction.time.strftime('%d/%m/%Y')))
           concat(content_tag(:td, transaction.time.strftime('%H:%M')))
           concat(content_tag(:td, transaction.memo))
-          concat(content_tag(:td, number_to_currency(transaction.amount)))
+          concat(content_tag(:td, transaction.amount, class: 'numeric'))
+          concat(content_tag(:td, number_to_currency(transaction.fiat), class: 'numeric'))
+          concat(content_tag(:td, number_to_currency(transaction.price_at_time), class: 'numeric'))
           concat(content_tag(:td, link_to('View', transaction_path(transaction))))
         end)
       end
