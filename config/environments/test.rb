ae55bfce5a38bd2c6bdfee3a4b5f82763df7702f
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-07 14:57:49 +0000

    Turn off logging in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Set log level to warn to reduce noisy output from tests
+  config.log_level = :warn
 end
