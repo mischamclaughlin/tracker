ac6babae4f52128e617ff79732902f5d8b5b1e7d
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-07 14:56:47 +0000

    Change rounding logic to prefer '%.2f'

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,21 +1,21 @@
 module PortfoliosHelper
   def total_portfolios_value(portfolios)
-    portfolios.sum(&:current_value).round(2)
+    portfolios.sum(&:current_value)
   end
 
   def total_portfolios_invested(portfolios)
-    portfolios.sum(&:total_fiat_invested).round(2)
+    portfolios.sum(&:total_fiat_invested)
   end
 
   def total_portfolios_profit_loss(portfolios)
-    portfolios.sum(&:profit_loss).round(2)
+    portfolios.sum(&:profit_loss)
   end
 
   def total_portfolios_profit_loss_percentage(portfolios)
     total_invested = total_portfolios_invested(portfolios)
     return 0 if total_invested.zero?
     
-    ((total_portfolios_profit_loss(portfolios) / total_invested) * 100).round(2)
+    ((total_portfolios_profit_loss(portfolios) / total_invested) * 100)
   end
 
   def display_portfolios(portfolios)
@@ -33,7 +33,7 @@ module PortfoliosHelper
         concat(content_tag(:td, number_to_currency(total_portfolios_value(portfolios)), class: 'numeric main'))
         concat(content_tag(:td, number_to_currency(total_portfolios_invested(portfolios)), class: 'numeric main'))
         concat(content_tag(:td, content_tag(:div, number_to_currency(total_portfolios_profit_loss(portfolios)), class: total_portfolios_profit_loss(portfolios).positive? ? 'main positive numeric' : 'main negative numeric')))
-        concat(content_tag(:td, content_tag(:div, "#{total_portfolios_profit_loss_percentage(portfolios)}%", class: total_portfolios_profit_loss_percentage(portfolios).positive? ? 'main positive numeric' : 'main negative numeric')))
+        concat(content_tag(:td, content_tag(:div, "#{'%.2f' % total_portfolios_profit_loss_percentage(portfolios)}%", class: total_portfolios_profit_loss_percentage(portfolios).positive? ? 'main positive numeric' : 'main negative numeric')))
         concat(content_tag(:td, '---', class: 'main'))
       end)
       concat(content_tag(:tr) do
@@ -44,8 +44,8 @@ module PortfoliosHelper
           concat(content_tag(:td, portfolio.portfolio_name))
           concat(content_tag(:td, number_to_currency(portfolio.current_value), class: 'numeric'))
           concat(content_tag(:td, number_to_currency(portfolio.total_fiat_invested), class: 'numeric'))
-          concat(content_tag(:td, content_tag(:div, number_to_currency(portfolio.profit_loss.round(2)), class: portfolio.profit_loss.positive? ? 'positive numeric' : 'negative numeric')))
-          concat(content_tag(:td, content_tag(:div, "#{portfolio.profit_loss_percentage}%", class: portfolio.profit_loss_percentage.positive? ? 'positive numeric' : 'negative numeric')))
+          concat(content_tag(:td, content_tag(:div, number_to_currency(portfolio.profit_loss), class: portfolio.profit_loss.positive? ? 'positive numeric' : 'negative numeric')))
+          concat(content_tag(:td, content_tag(:div, "#{'%.2f' % portfolio.profit_loss_percentage}%", class: portfolio.profit_loss_percentage.positive? ? 'positive numeric' : 'negative numeric')))
           concat(content_tag(:td, link_to('View', portfolio_path(portfolio))))
         end)
       end
