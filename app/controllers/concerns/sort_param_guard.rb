ef5a790d35e62a82fb5b351b6483fa41f0c8f263
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-10 15:37:14 +0000

    Fix logic and apply new constraints

--- app/controllers/concerns/sort_param_guard.rb
+++ app/controllers/concerns/sort_param_guard.rb
@@ -8,16 +8,53 @@ module SortParamGuard
   private
 
   def check_sort_params
-    default_param = controller_name == 'transactions' ? 'time' : 'balance_in_fiat'
-    default_path = controller_name == 'transactions' ? :transactions_path : :assets_path
-    unless params[:sort_by].present?
-      params[:sort_by] = default_param
+    case controller_name
+    when 'transactions'
+      params_info = set_transaction_params
+    when 'portfolios'
+      params_info = set_portfolio_params
+    when 'coins'
+      params_info = set_coin_params
+    else
+      log_error("SortParamGuard: Unknown controller #{controller_name}")
+      return
     end
 
-    allowed_columns = controller_name == 'transactions' ? Transaction::ALLOWED_COLUMNS : Asset::ALLOWED_COLUMNS
+    unless params[:sort_by].present?
+      params[:sort_by] = params_info[:default_param]
+      params[:dir] ||= 'desc'
+      log_info("No sort parameter provided, defaulting to '#{params[:sort_by]}'")
+      return
+    end
+
+    allowed_columns = params_info[:allowed_columns]
     unless allowed_columns.include?(params[:sort_by])
-      log_info("Invalid sort parameter: #{params[:sort_by]}, reset to default '#{default_param}'")
-      redirect_to send(default_path, sort_by: default_param), alert: 'Invalid sort parameter.'
+      log_info("Invalid sort parameter: #{params[:sort_by]}, reset to default '#{params_info[:default_param]}'")
+      redirect_to send(params_info[:default_path], sort_by: params_info[:default_param]), alert: 'Invalid sort parameter.'
+    end
+  end
+
+  def set_transaction_params
+    default_param = 'time'
+    default_path = :transactions_path
+    { default_param: default_param, default_path: default_path, allowed_columns: Transaction::ALLOWED_COLUMNS }
+  end
+
+  def set_portfolio_params
+    default_param = 'portfolio_name'
+    default_path = :portfolios_path
+    { default_param: default_param, default_path: default_path, allowed_columns: Portfolio::ALLOWED_COLUMNS }
+  end
+
+  def set_coin_params
+    default_param = 'coin_name'
+    default_path = :coins_path
+    { default_param: default_param, default_path: default_path, allowed_columns: Coin::ALLOWED_COLUMNS }
+  end
+
+  def switch_sort_direction(dir, sort_by)
+    if sort_by == 'coin_name' || sort_by == 'symbol' || sort_by == 'portfolio_name'
+      dir == 'asc' ? 'desc' : 'asc'
     end
   end
 end
--- app/models/concerns/safe_ordering.rb
+++ app/models/concerns/safe_ordering.rb
@@ -2,9 +2,14 @@ module SafeOrdering
   extend ActiveSupport::Concern
 
   class_methods do
-    def order_by_column(column, direction = :desc)
+    def order_by_column(column, direction = :asc)
       safe_column = self::ALLOWED_COLUMNS.include?(column) ? column : self::ALLOWED_COLUMNS.first
-      safe_direction = %w[asc desc].include?(direction.to_s.downcase) ? direction : :desc
+      safe_direction = %w[asc desc].include?(direction.to_s.downcase) ? direction : :asc
+
+      if self::ALPHA_COLS.include?(safe_column)
+        safe_direction = safe_direction.to_s.downcase == 'asc' ? 'desc' : 'asc'
+      end
+
       order(Arel.sql("#{safe_column} #{safe_direction}"))
     end
   end
