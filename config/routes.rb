26e90548a9bf1e76a4905ad77d0f4cf5277fc5fb
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-12 13:08:42 +0000

    Add root path and login/logout functionality to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,7 @@ Rails.application.routes.draw do
   resources :coins
   resources :portfolios
   resources :transactions
-  # resources :assets, only: [:index, :show], path: 'coins', param: :name do
-  #   member do
-  #     post :recalculate_balance
-  #   end
-  #   collection do
-  #   end
-  # end
+  root to: 'portfolios#index'
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
