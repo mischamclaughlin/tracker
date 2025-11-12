5935ae5dfac26eccd3a41e88548b0a1159191a5f
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-12 12:48:19 +0000

    Setup users and ensure users porfolio/transactions are used for them

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   include LoggingModule
   
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   allow_browser versions: :modern
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username])
+  end
 end
