69d3ea2fae8e856b1b824fa1aafc81517e1d578c
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-12 17:29:20 +0000

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :holdings
   resources :coins
   resources :portfolios
   resources :transactions
+
   root to: 'portfolios#index'
 
+  get 'quick_add', to: 'quick_add#new', as: :quick_add
   get "up" => "rails/health#show", as: :rails_health_check
 end
