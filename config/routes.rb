b3100b550ba3111b277dd965c7ea2a3015f78341
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-13 15:41:40 +0000

    Polish code in accordance to rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   resources :portfolios
   resources :transactions
 
-  root to: 'portfolios#index'
+  root to: "portfolios#index"
 
-  get 'quick_add', to: 'quick_add#new', as: :quick_add
+  get "quick_add", to: "quick_add#new", as: :quick_add
   get "up" => "rails/health#show", as: :rails_health_check
 end
