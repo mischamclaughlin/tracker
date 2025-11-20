f7cefec9419b0b669450162e583265a2b39dc00f
Mischa McLaughlin <mischa.mclaughlin@sage.com>
2025-11-20 13:33:25 +0000

    Js files and required imports

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,5 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
+// Register controllers explicitly to avoid dynamic import issues with eager loader
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import PriceChartController from "controllers/price_chart_controller"
+
+application.register("price-chart", PriceChartController)
